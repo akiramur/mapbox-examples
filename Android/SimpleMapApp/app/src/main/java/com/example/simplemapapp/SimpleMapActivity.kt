be96b4a8f6c051b320e6e3b4739b4dc112bd4bd2
Akira Murao <3124453+akiramur@users.noreply.github.com>
2025-01-15 14:25:27 +0900

    add SimpleMap-button

--- Android/SimpleMapApp/app/src/main/java/com/example/simplemapapp/SimpleMapActivity.kt
+++ Android/SimpleMapApp/app/src/main/java/com/example/simplemapapp/SimpleMapActivity.kt
@@ -1,34 +1,48 @@
 package com.example.simplemapapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.simplemapapp.databinding.ActivitySimpleMapBinding
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.scalebar.scalebar
 
-/**
- * Example of displaying a map.
- */
 class SimpleMapActivity : AppCompatActivity() {
+
+    lateinit var mapView: MapView
+    private lateinit var binding: ActivitySimpleMapBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val mapView = MapView(this)
-        setContentView(mapView)
-	
+        binding = ActivitySimpleMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mapView = binding.mapView
         mapView.mapboxMap
-          .apply {
-            setCamera(
-              CameraOptions.Builder()
-                .center(Point.fromLngLat(LONGITUDE, LATITUDE))
-                .zoom(9.0)
-                .build()
-            )
-          }
+            .apply {
+                setCamera(
+                    CameraOptions.Builder()
+                        .center(Point.fromLngLat(LONGITUDE, LATITUDE))
+                        .zoom(9.0)
+                        .build()
+                )
+            }
+
+        mapView.scalebar.enabled = false
+        mapView.mapboxMap.loadStyle(Style.STANDARD)
+
+        binding.button.setOnClickListener {
+            Log.d(TAG, "button clicked")
+        }
     }
 
     companion object {
+        private val TAG = "SimpleMapActivity"
         private const val LATITUDE = 40.0
         private const val LONGITUDE = -74.5
     }
